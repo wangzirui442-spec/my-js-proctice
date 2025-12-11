ff66706d8d538ea9194adb8674a75c373adf8480
RUI <aruiarui0217@qq.com>
2025-12-11 20:22:28 +0800

    第一次打代码

---
+++ index.js
@@ -0,0 +1,11 @@
+let age = 91;
+console.log(age);
+age = 78;
+console.log(age);
+let $power = 200000;
+let $Power = 100000;
+console.log($power);
+console.log($Power);
+const max_health = 999999;//这是一个常量
+let levels = 75;//这是一个变量
+console.log('I Love China');

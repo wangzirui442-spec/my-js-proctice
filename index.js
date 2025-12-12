06c9fab6b70619864afde4ac711bbb70d380556f
王梓睿 <aruiarui0217@qq.com>
2025-12-12 20:59:51 +0800

    完成了字符串练习

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ console.log($Power);
 const max_health = 999999;//这是一个常量
 let levels = 75;//这是一个变量
 console.log('I Love China');
+//                                                     Day 2                                                                 //
+let greeting = 'Hello';
+let target ='Web3';
+let result = greeting + ' ' + target;
+console.log(result);
+let sentence = 'Today';
+sentence += ',I leaned programming';
+console.log(sentence);
+let part1 = 'Web3';
+let part2 = 'is very cool';
+let part3 = part1.cancat(' ', part2);
+console.log(part3);

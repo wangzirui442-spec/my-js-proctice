420041c03bfa0da22658d4a2dfb341219a55c294
wangzirui442-spec <wangzirui442@gmail.com>
2026-01-12 19:34:52 +0800

    replace

---
+++ strings/replace/replace.js
@@ -0,0 +1,12 @@
+let text = "I love JavaScript!";
+let newText = text.replace("JavaScript", "coding");
+console.log(newText);
+//case-sensitive
+let s = "I enjoy fresh air.";
+s.replace("Air", "water");
+//defalut replace the first occurrence
+let a = "Hi, world! Welcome to the Web3 World.";
+let updated = a.replace("world", "universe");
+console.log(updated);
+
+

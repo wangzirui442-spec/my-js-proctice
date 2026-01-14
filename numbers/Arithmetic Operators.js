85832f50284788aecbb296d9476379a5b1f7dc91
wangzirui442-spec <wangzirui442@gmail.com>
2026-01-14 22:12:04 +0800

    Proctice Number type and arithmentic oprators

---
+++ numbers/Arithmetic Operators.js
@@ -0,0 +1,36 @@
+
+//the simple arithmetic operator
+
+let x = 12;
+let y = 5;
+console.log = (x + y);
+console.log = (x - y);
+console.log = (x * y);
+console.log = (x / y);
+
+//Infinity
+
+let result = 10 / 0;
+console.log(result);
+console.log(typeof result);
+
+//return the remainder of a division
+
+let num = 7;
+if (num % 2 === 0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
+
+//exponentiation operator
+
+console.log(2**3);
+console.log(5**2);
+
+//mix operators and operator precedence
+
+let result2 = 10 + 5 * 2 - 8 / 4 ;
+console.log(result2);
+
+
---
+++ numbers/number type.js
@@ -0,0 +1,13 @@
+//the number including value
+const a = 10;
+const b = 3.1415926;
+const c = -5;
+const d = 1 / 0;
+const e = "Hello" / 2;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof e);
+
+
+

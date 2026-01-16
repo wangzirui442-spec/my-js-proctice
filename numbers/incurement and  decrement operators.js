dbcef23b6ef90b7c64cd4af70ec629d9cc93adf8
wangzirui442-spec <wangzirui442@gmail.com>
2026-01-16 21:58:13 +0800

    incurement and decrement operators

---
+++ numbers/incurement and  decrement operators.js
@@ -0,0 +1,25 @@
+let x = 5;
+console.log(--x);
+console.log(x);
+
+let y = 5;
+console.log(y--);
+console.log(y);
+
+//prefix operators
+let a = 6;
+console.log(++a);
+console.log(a);
+//postfix operaters
+let s = 5;
+console.log(s++);
+console.log(s);
+//
+let d = 8;
+let e = ++d;
+console.log(e);
+
+let j = 99;
+let u = 99++;
+console.log(u);
+

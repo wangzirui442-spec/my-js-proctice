e209dd25ea810e4e2c9c2e4871967e70315137ca
wangzirui442-spec <wangzirui442@gmail.com>
2026-02-01 21:56:55 +0800

    learn Compound Assignment Operators

---
+++ numbers/Compound Assignment Operators.js
@@ -0,0 +1,26 @@
+//   /=   Division Assignment operator
+let balance = 100;
+balance /= 4;
+console.log(balance);//the same as:  balance = balance / 4
+
+//   %=   Bemainder Assignment operator
+let num = 10;
+num %= 3;
+console.log(num);//the same as:  num = num %=3
+
+//   **=  Exponent Assignment operator
+let power = 2;
+power **= 3;
+console.log(power);//the same as: power = power **3
+
+//   &=   Bitwise AND Assignment operator
+let a = 5;
+a &= 1;
+
+//    |=     Bitwise OR Assignment operator
+let b = 5;
+b |= 1;
+
+
+
+

3c864a43acae982e73ed5d9907fbec6eb868f19c
wangzirui442-spec <wangzirui442@gmail.com>
2026-02-02 21:42:56 +0800

    learn Booleans and Equality / Inequality Operators

---
+++ Boooleans/Equality   Inequality.js
@@ -0,0 +1,15 @@
+//if/else 
+if (isOldEnoughToDrive){
+    console.log ("You are old enough to drive");
+}
+else{console.log('Sorry you are not old enough to drive');
+}
+//Equality Operator 
+console.log(9 == "9");   // true  
+//Strict Equality Operator
+console.log(9==="9");     //false
+
+//Inequality Operator
+console.log(0 != "0");   //false
+//Strict Inequality Operator
+console.log(1 !== "1");   //true
